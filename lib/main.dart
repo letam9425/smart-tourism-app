0b8688abe23f2422d045a00bd990acd93ce659f4
DESKTOP-F0U7RVS <lenguyenminhtamyokid@gmail.com>
2026-03-10 19:18:31 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Smart Tourism appp',
+      title: 'Smart Tourism app',
       theme: ThemeData(
         // This is the theme of your application.
         //
